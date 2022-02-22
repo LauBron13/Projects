24dc52b1f6ef537f9a99c9084a2118c3db949d18
LauBron13 <99703250+LauBron13@users.noreply.github.com>
2022-02-22 18:56:44 +0800

    Add files via upload
    js

---
+++ Modal/app.js
@@ -0,0 +1,7 @@
+// select modal-btn,modal-overlay,close-btn
+
+const modalBtn = document.querySelector
+
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
